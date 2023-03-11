c9b6c2e2ed508b2a3cbd0ee63d44a23bf170b72c
thochit <thochit2762003@gmail.com>
2023-03-11 07:48:14 +0700

    add assignment

---
+++ Assignment/Input_Image.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+
+def input_image():
+    # Get the name and id of the student from the user
+    name = input("Enter the name of student: ")
+    id = input("Enter the ID of student: ")
+    print('Look at the Camera')
+
+    # Create a directory for the student's images
+    if not os.path.exists(f"Student_Image/{name}_{id}"):
+        os.makedirs(f"Student_Image/{name}_{id}")
+
+    # Open the webcam
+    cap = cv2.VideoCapture(0)
+
+    # Loop over frames from the webcam and save 10 images
+    for j in range(100):
+        # Capture a frame from the webcam
+        ret, frame = cap.read()
+
+        # If the frame cannot be captured, break out of the loop
+        if not ret:
+            break
+
+        # Save the image to a file
+        file_path = f"Student_Image/{name}_{id}/{name}_{id}_{j}.jpg"
+        cv2.imwrite(file_path, frame)
+
+        # Display the image
+        cv2.imshow('image', frame)
+        cv2.waitKey(50)
+
+    # Release the webcam and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+input_image()
---
+++ Assignment/Regconition.py
@@ -0,0 +1,124 @@
+import os
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.datasets import fetch_lfw_people
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import ConfusionMatrixDisplay
+
+# Define the directory where the student images are stored
+input_dir = "Student_Image"
+
+# Get a list of all the student names in the input directory
+students = os.listdir(input_dir)
+data = []
+
+# Loop over all the student images and recognize faces
+for student in students:
+    # Get a list of all the image files for the current student
+    images = os.listdir(os.path.join(input_dir, student))
+    name, id = student.split('_')
+
+    # Loop over all the image files and recognize faces
+    for image_file in images:
+        # Load the image
+        image_path = os.path.join(input_dir, student, image_file)
+        image = cv2.imread(image_path)
+        #Store image and student
+        data.append((image, student))
+
+    # Print the student name and predicted label
+    print(f"Student: {name}, ID: {id}")
+
+#Initalize face list
+faces_list = []
+size = (64, 64)  # desired size of the resized images
+
+
+for image, student in data:
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Load the face detection classifier
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Detect faces in the image
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+
+    # Loop over the detected faces and extract each one
+    for (x, y, w, h) in faces:
+        # Crop the image around the face
+        face_img = image[y:y + h, x:x + w]
+
+        # Resize the face image
+        face_img = cv2.resize(face_img, size)
+
+        #Store face of each studentq
+        faces_list.append((face_img, student))
+
+        # Display the extracted face
+        # cv2.imshow(student, face_img)
+        # cv2.waitKey(0)
+
+# Create an empty list to store the face images and their corresponding labels
+face_images = []
+face_labels = []
+
+#Load face and labels
+for face, label in faces_list:
+    # Append the image and its label to the lists
+    face_images.append(face)
+    face_labels.append(label)
+
+face_images = np.array(face_images)
+face_labels = np.array(face_labels)
+
+# print(face_images.shape)
+
+def plot_gallery(images, titles, h=480, w=640, n_row=3, n_col=4):
+    """Helper function to plot a gallery of portraits"""
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=0.01, right=0.99, top=0.90, hspace=0.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i], cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+    plt.show()
+
+# plot_gallery(face_images, face_labels)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(face_images, face_labels, test_size=0.2, random_state=42)
+
+# Flatten the 2D images to 1D arrays
+X_train_flat = X_train.reshape(X_train.shape[0], -1)
+X_test_flat = X_test.reshape(X_test.shape[0], -1)
+
+# Scale the data using the same scaler used on the training set
+scaler = StandardScaler()
+X_test_scaled = scaler.fit_transform(X_test_flat)
+
+# Perform PCA to reduce the dimensionality of the data
+n_components = 2  # Number of principal components to keep
+pca = PCA(n_components=n_components, whiten=True, random_state=42)
+X_train_pca = pca.fit_transform(X_train_flat)
+X_test_pca = pca.transform(X_test_flat)
+
+# Train an SVM classifier on the training data
+svm = SVC(kernel='rbf', C=10, gamma=0.001, random_state=42)
+svm.fit(X_train_pca, y_train)
+
+# Evaluate the performance of the model on the testing data
+y_pred = svm.predict(X_test_pca)
+print(classification_report(y_test, y_pred))
+
+
+
