964f7fd5e2a143ac76468fe022dd4fe53b4ee1b3
pphuc25 <phanphuc1100@gmail.com>
2023-03-15 18:33:01 +0700

    refactor: add the K-means (function 3)

--- workshop4/Lab4.py
+++ workshop4/Lab4.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from skimage.transform import resize
+import matplotlib.pyplot as plt
 import math
 from pylab import*
 from typing import Any
@@ -104,30 +105,146 @@ def watershed(img):
     cv2.waitKey(0)  # Wait for any key press
     cv2.destroyAllWindows()  # Close all windows
 
-def kmeans_segmentation(img, k=4, n_iter=10):
+# def kmeans_segmentation(img, k=4, n_iter=10):
 
-    # Reshape image
-    pixel_values = img.reshape((-1, 3))
-    pixel_values = np.float32(pixel_values)
+#     # Reshape image
+#     pixel_values = img.reshape((-1, 3))
+#     pixel_values = np.float32(pixel_values)
 
-    # Define stopping criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, n_iter, 1.0)
+#     # Define stopping criteria
+#     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, n_iter, 1.0)
 
-    # Perform k-means clustering
-    _, labels, centers = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+#     # Perform k-means clustering
+#     _, labels, centers = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-    # Convert data back to 8-bit values
-    centers = np.uint8(centers)
-    segmented_data = centers[labels.flatten()]
+#     # Convert data back to 8-bit values
+#     centers = np.uint8(centers)
+#     segmented_data = centers[labels.flatten()]
 
-    # Reshape data back to the original image dimensions
-    segmented_image = segmented_data.reshape(img.shape)
+#     # Reshape data back to the original image dimensions
+#     segmented_image = segmented_data.reshape(img.shape)
 
-    # Display original and segmented images
-    cv2.imshow("Original Image", img)
-    cv2.imshow("Segmented Image", segmented_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+#     # Display original and segmented images
+#     cv2.imshow("Original Image", img)
+#     cv2.imshow("Segmented Image", segmented_image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+np.random.seed(42)
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2)**2))
+
+class KMeans():
+
+    def __init__(self, image, K=5, max_iters=100, plot_steps=False):
+        self.image = image
+        self.K = K
+        self.max_iters = max_iters
+        self.plot_steps = plot_steps
+
+        # list of sample indices for each cluster
+        self.clusters = [[] for _ in range(self.K)]
+        # the centers (mean feature vector) for each cluster
+        self.centroids = []
+
+    def predict(self, X):
+        self.X = X
+        print(self.X.shape)
+        self.n_samples, self.n_features = X.shape
+        
+        # initialize 
+        random_sample_idxs = np.random.choice(self.n_samples, self.K, replace=False)
+        self.centroids = [self.X[idx] for idx in random_sample_idxs]
+
+        # Optimize clusters
+        for _ in range(self.max_iters):
+            # Assign samples to closest centroids (create clusters)
+            self.clusters = self._create_clusters(self.centroids)
+            if self.plot_steps:
+                self.plot()
+
+            # Calculate new centroids from the clusters
+            centroids_old = self.centroids
+            self.centroids = self._get_centroids(self.clusters)
+            
+            # check if clusters have changed
+            if self._is_converged(centroids_old, self.centroids):
+                break
+
+            if self.plot_steps:
+                self.plot()
+
+        # Classify samples as the index of their clusters
+        return self._get_cluster_labels(self.clusters)
+
+
+    def _get_cluster_labels(self, clusters):
+        # each sample will get the label of the cluster it was assigned to
+        labels = np.empty(self.n_samples)
+
+        for cluster_idx, cluster in enumerate(clusters):
+            for sample_index in cluster:
+                labels[sample_index] = cluster_idx
+        return labels
+
+    def _create_clusters(self, centroids):
+        # Assign the samples to the closest centroids to create clusters
+        clusters = [[] for _ in range(self.K)]
+        for idx, sample in enumerate(self.X):
+            centroid_idx = self._closest_centroid(sample, centroids)
+            clusters[centroid_idx].append(idx)
+        return clusters
+
+    def _closest_centroid(self, sample, centroids):
+        # distance of the current sample to each centroid
+        distances = [euclidean_distance(sample, point) for point in centroids]
+        closest_index = np.argmin(distances)
+        return closest_index
+
+    def _get_centroids(self, clusters):
+        # assign mean value of clusters to centroids
+        centroids = np.zeros((self.K, self.n_features))
+        for cluster_idx, cluster in enumerate(clusters):
+            cluster_mean = np.mean(self.X[cluster], axis=0)
+            centroids[cluster_idx] = cluster_mean
+        return centroids
+
+    def _is_converged(self, centroids_old, centroids):
+        # distances between each old and new centroids, fol all centroids
+        distances = [euclidean_distance(centroids_old[i], centroids[i]) for i in range(self.K)]
+        return sum(distances) == 0
+
+    def plot(self):
+        fig, ax = plt.subplots(figsize=(12, 8))
+
+        for i, index in enumerate(self.clusters):
+            point = self.X[index].T
+            ax.scatter(*point)
+
+        for point in self.centroids:
+            ax.scatter(*point, marker="x", color='black', linewidth=2)
+
+        plt.show()
+    def cent(self):
+        return self.centroids
+    
+    def run(self):
+        pixel_values = self.image.reshape((-1, 3))
+        pixel_values = np.float32(pixel_values)
+        k = KMeans(self.image, K=4, max_iters=10)
+        y_pred = k.predict(pixel_values)
+        y_pred = y_pred.astype(int)
+
+        k.cent()
+        centers = np.uint8(k.cent())
+        labels = y_pred.flatten()
+        segmented_image = centers[labels.flatten()]
+        segmented_image = segmented_image.reshape(self.image.shape)
+        cv2.imshow('original image', self.image)
+        cv2.imshow('new image', segmented_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 def mean_shift_peak_detection(K):
@@ -299,7 +416,7 @@ def main():
     print("Please select a function from the following options:")
 
     while True:
-        image_path = 'road.jpg'
+        image_path = 'workshop4/road.jpg'
         image = cv2.imread(image_path)
         print("1. Snakes Algorithm for Active Contours")
         print("2. Watershed Algorithm for Image Segmentation")
@@ -318,7 +435,7 @@ def main():
 
         elif choice == 3:
             print("K-Means for Segmentation selected.")
-            kmeans_segmentation(image)
+            KMeans(image, K=4, max_iters=10).run()
 
 
         elif choice == 4:
